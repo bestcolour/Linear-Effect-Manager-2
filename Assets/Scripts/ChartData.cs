33e1cd496c1df51d6a06082859ad1b67be70834c
bestcolour <guest3050@gmail.com>
2020-09-28 10:54:58 +0800

    Clear

--- Assets/Scripts/ChartData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Chart Data", menuName = "LEM/ChartData")]
-public class ChartData : ScriptableObject
-{
-    public int Count = 0;
-}
