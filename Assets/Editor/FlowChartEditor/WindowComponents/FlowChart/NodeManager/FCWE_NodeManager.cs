dc18178df453ba75813c05bb42c81c2de556a083
bestcolour <guest3050@gmail.com>
2020-12-20 10:50:57 +0800

    Shifted drawing code into a new cs file

--- Assets/Editor/FlowChartEditor/WindowComponents/FlowChart/NodeManager/FCWE_NodeManager.cs
+++ Assets/Editor/FlowChartEditor/WindowComponents/FlowChart/NodeManager/FCWE_NodeManager.cs
@@ -16,15 +16,6 @@
         public delegate void ClickOnBlockNodeCallback(BlockNode block);
         #endregion
 
-        #region Statics
-        static GUIStyle DebugStyle;
-        static GUIContent DebugGUIContent;
-        #endregion
-
-        #region Constants
-        static readonly Color SELECTIONBOX_COLOUR = new Color(.75f, .93f, .93f, 0.5f);
-        #endregion
-
         #region Events
         public event ClickOnBlockNodeCallback OnSelectBlockNode = null;
         public event Action OnNoBlockNodeFound = null;
@@ -57,7 +48,7 @@
         private void NodeManager_OnEnable()
         {
             _selectedBlocks = new HashSet<BlockNode>();
-            NodeManager_InitializeDebugger();
+            NodeManager_Drawer_OnEnable();
             NodeManager_SaveManager_OnEnable();
             _selectedBlockIndex = -1;
             _dragState = DragState.Default;
@@ -87,71 +78,13 @@
             Event e = Event.current;
 
             NodeManager_NodeCycler_OnGUI();
-
-            NodeManager_Draw();
-            NodeManager_DrawDebugger();
+            NodeManager_Drawer_OnGUI();
         }
 
-        void NodeManager_InitializeDebugger()
-        {
-            DebugStyle = new GUIStyle();
-            DebugStyle.wordWrap = true;
-            DebugStyle.normal.textColor = Color.red;
-            DebugGUIContent = new GUIContent();
-        }
+
 
         #endregion
-        #region Draw
 
-        void NodeManager_Draw()
-        {
-            //========== DRAW NODE BLOCKS ===========
-            for (int i = 0; i < _allBlockNodes.Count; i++)
-            {
-                _allBlockNodes[i].DrawNodeArrowLines();
-            }
-
-            //Draw from the bottom up
-            for (int i = 0; i < _allBlockNodes.Count; i++)
-            {
-                _allBlockNodes[i].DrawNodeBlocks();
-            }
-
-            //Draw Selection box
-            if (_dragState == DragState.DrawSelection_HadDragged)
-            {
-                Event e = Event.current;
-                _selectionBox.width = e.mousePosition.x - _selectionBox.x;
-                _selectionBox.height = e.mousePosition.y - _selectionBox.y;
-
-                Color prevColour = GUIExtensions.Start_GUI_ColourChange(SELECTIONBOX_COLOUR);
-                GUI.Box(_selectionBox, string.Empty);
-                GUIExtensions.End_GUI_ColourChange(prevColour);
-            }
-
-        }
-
-        void NodeManager_DrawDebugger()
-        {
-            Rect rect = Rect.zero;
-            //Abit of border
-            rect.x = 5f;
-            rect.y = TOOLBAR_HEIGHT;
-
-            string debugStatement = $"Number of selected blocks: {_selectedBlocks.Count} \n Drag State: {_dragState} ";
-
-
-            DebugGUIContent.text = debugStatement;
-            rect.size = DebugStyle.CalcSize(DebugGUIContent);
-
-            GUI.Label
-            (
-                rect,
-                DebugGUIContent,
-                DebugStyle
-            );
-        }
-        #endregion
         #region Event Handlers
         void NodeManager_HandlePan(Vector2 mouseDelta)
         {
@@ -207,9 +140,6 @@
                     }
                     break;
             }
-
-
-
         }
 
 
---
+++ Assets/Editor/FlowChartEditor/WindowComponents/FlowChart/NodeManager/FCWE_NodeManager_Drawer.cs
@@ -0,0 +1,99 @@
+﻿namespace LinearEffectsEditor
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEditor;
+    using LinearEffects;
+
+
+    public partial class FlowChartWindowEditor : EditorWindow
+    {
+  #region Statics
+        static GUIStyle DebugStyle;
+        static GUIContent DebugGUIContent;
+        #endregion
+
+
+        #region Constants
+        static readonly Color SELECTIONBOX_COLOUR = new Color(.75f, .93f, .93f, 0.5f);
+        #endregion
+
+
+
+
+        void NodeManager_Drawer_OnEnable()
+        {
+            NodeManager_InitializeDebugger();
+        }
+
+        void NodeManager_InitializeDebugger()
+        {
+            DebugStyle = new GUIStyle();
+            DebugStyle.wordWrap = true;
+            DebugStyle.normal.textColor = Color.red;
+            DebugGUIContent = new GUIContent();
+        }
+
+        void NodeManager_Drawer_OnGUI()
+        {
+            NodeManager_Draw();
+            NodeManager_DrawDebugger();
+        }
+
+
+        #region Draw Methods
+        void NodeManager_Draw()
+        {
+            //========== DRAW NODE BLOCKS ===========
+            for (int i = 0; i < _allBlockNodes.Count; i++)
+            {
+                _allBlockNodes[i].DrawNodeArrowLines();
+            }
+
+            //Draw from the bottom up
+            for (int i = 0; i < _allBlockNodes.Count; i++)
+            {
+                _allBlockNodes[i].DrawNodeBlocks();
+            }
+
+            //Draw Selection box
+            if (_dragState == DragState.DrawSelection_HadDragged)
+            {
+                Event e = Event.current;
+                _selectionBox.width = e.mousePosition.x - _selectionBox.x;
+                _selectionBox.height = e.mousePosition.y - _selectionBox.y;
+
+                Color prevColour = GUIExtensions.Start_GUI_ColourChange(SELECTIONBOX_COLOUR);
+                GUI.Box(_selectionBox, string.Empty);
+                GUIExtensions.End_GUI_ColourChange(prevColour);
+            }
+
+        }
+
+        void NodeManager_DrawDebugger()
+        {
+            Rect rect = Rect.zero;
+            //Abit of border
+            rect.x = 5f;
+            rect.y = TOOLBAR_HEIGHT;
+
+            string debugStatement = $"Number of selected blocks: {_selectedBlocks.Count} \n Drag State: {_dragState} ";
+
+
+            DebugGUIContent.text = debugStatement;
+            rect.size = DebugStyle.CalcSize(DebugGUIContent);
+
+            GUI.Label
+            (
+                rect,
+                DebugGUIContent,
+                DebugStyle
+            );
+        }
+        #endregion
+
+
+    }
+
+}
