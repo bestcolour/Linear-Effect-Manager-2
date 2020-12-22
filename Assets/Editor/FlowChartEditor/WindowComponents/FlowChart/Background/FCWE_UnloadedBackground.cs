315759c9f0edd8b9ed2425605f4210974f8f7d7f
bestcolour <guest3050@gmail.com>
2020-12-22 17:55:06 +0800

    Fixed windows missing instance when i drag drop into obj field of unloaded bg

--- Assets/Editor/FlowChartEditor/WindowComponents/FlowChart/Background/FCWE_UnloadedBackground.cs
+++ Assets/Editor/FlowChartEditor/WindowComponents/FlowChart/Background/FCWE_UnloadedBackground.cs
@@ -9,13 +9,15 @@
     {
         void UnloadedBackground_OnGUI()
         {
-            EditorGUILayout.BeginVertical();EditorGUILayout.LabelField(string.Empty);EditorGUILayout.EndVertical();
+            EditorGUILayout.BeginVertical(); EditorGUILayout.LabelField(string.Empty); EditorGUILayout.EndVertical();
             Rect rect = GUILayoutUtility.GetLastRect();
             _flowChart = (BaseFlowChart)EditorGUI.ObjectField(rect, "Target FlowChart", _flowChart, typeof(BaseFlowChart), true);
+            instance = GetWindow<FlowChartWindowEditor>();
             if (_flowChart != null)
             {
                 REINITIALIZE();
             }
+            
         }
     }
 
