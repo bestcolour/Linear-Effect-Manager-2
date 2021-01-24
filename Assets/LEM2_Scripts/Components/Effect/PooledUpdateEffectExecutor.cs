5887e5f24ebdb2343f32451da8f26b0b1f47856f
bestcolour <guest3050@gmail.com>
2021-01-24 11:38:00 +0800

    Update PooledUpdateEffectExecutor.cs

--- Assets/LEM2_Scripts/Components/Effect/PooledUpdateEffectExecutor.cs
+++ Assets/LEM2_Scripts/Components/Effect/PooledUpdateEffectExecutor.cs
@@ -4,7 +4,7 @@
     using UnityEngine;
 
     ///<Summary>This class takes in a T generic type which inherits from UpdateEffectWithRuntimeData class and a RuntimeData type which will be pooled during runtime so that effect calls can be more optimised by reusing RuntimeData objects. Since the UpdateEffectWithRuntimeData class uses a pointer to reference the RuntimeData instance, the pointer size is 4 bytes on a 32 OS and 8 bytes on a 64 OS. Use this if you have runtime variables exceeding 4/8 bytes</Summary>
-    public abstract class PooledUpdateEffectExecutor<Effect, RuntimeData> : EffectExecutor<Effect>
+    public abstract class PooledUpdateEffectExecutor<Effect, RuntimeData> : UpdateEffectExecutor<Effect>
     where Effect : UpdateEffectWithRuntimeData<RuntimeData>, new()
     where RuntimeData : class, new()
     {
@@ -38,17 +38,17 @@
 
 
         ///<Summary>Removes the runtime data reference from the UpdateEffect and returns it to the pool so that other effects can reuse the runtime data.</Summary>
-        protected virtual void EndExecuteEffect(Effect t)
+        protected override void EndExecuteEffect(Effect t)
         {
-            t.FirstFrameCall = false;
+            base.EndExecuteEffect(t);
             ReturnRuntimeData(t.RuntimeData);
             t.RuntimeData = null;
         }
 
         ///<Summary>Adds a runtime data instance to an UpdateEffect instance. Always call this method's base before writing your overrides</Summary>
-        protected virtual void BeginExecuteEffect(Effect t)
+        protected override void BeginExecuteEffect(Effect t)
         {
-            t.FirstFrameCall = true;
+            base.BeginExecuteEffect(t);
             t.RuntimeData = GetRuntimeData();
         }
 
