b8513c914296683418245d9d8d8939ee94e37c2d
bestcolour <guest3050@gmail.com>
2021-03-12 11:08:23 +0800

    Added the respective executors to the previously added 2 events

---
+++ Assets/LEM2_Scripts/Library/ScriptableEvents/SOEvent_RVoid_Bool_Executor.cs
@@ -0,0 +1,23 @@
+﻿namespace LinearEffects.ScriptableEvents
+{
+    using ScriptableObjectEvents;
+    [System.Serializable]
+    ///<Summary>Raises a scriptableobject event which returns void and have a int parameter with the value inputed in inspector</Summary>
+    public class SOEvent_RVoid_Bool_Executor : EffectExecutor<SOEvent_RVoid_Bool_Executor.MyEffect>
+    {
+        [System.Serializable]
+        public class MyEffect : Effect
+        {
+            public SOEvent_RVoid_Bool VoidEvent = default;
+            public bool Value = default;
+        }
+
+        protected override bool ExecuteEffect(MyEffect effectData)
+        {
+            effectData.VoidEvent.RaiseEvent(effectData.Value);
+            return true;
+        }
+
+    }
+
+}
---
+++ Assets/LEM2_Scripts/Library/ScriptableEvents/SOEvent_RVoid_String_Executor.cs
@@ -0,0 +1,23 @@
+﻿namespace LinearEffects.ScriptableEvents
+{
+    using ScriptableObjectEvents;
+    [System.Serializable]
+    ///<Summary>Raises a scriptableobject event which returns void and have a int parameter with the value inputed in inspector</Summary>
+    public class SOEvent_RVoid_String_Executor : EffectExecutor<SOEvent_RVoid_String_Executor.MyEffect>
+    {
+        [System.Serializable]
+        public class MyEffect : Effect
+        {
+            public SOEvent_RVoid_String VoidEvent = default;
+            public string Value = default;
+        }
+
+        protected override bool ExecuteEffect(MyEffect effectData)
+        {
+            effectData.VoidEvent.RaiseEvent(effectData.Value);
+            return true;
+        }
+
+    }
+
+}
